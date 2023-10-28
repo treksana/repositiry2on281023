0b8e613f417325d2469d01b0d6b868b3cd127a2f
treksana <147983343+treksana@users.noreply.github.com>
2023-10-28 07:33:03 +0500

    Add files via upload

---
+++ task6on281023.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, m = 0;
+	cout << "n = ";
+	cin >> n;
+	while n > 0:
+	{
+		if ((n % 10) > m) :
+			m=n%10;
+		n = n / 10;
+	}
+	cout << s;
+	return 0;
+}
